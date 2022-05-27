d1f27aadabe26f1a2cc3e7e80acd7de67eb37a3f
Rajat Mathew <rajatxmathew@gmail.com>
2022-05-28 02:19:07 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import App from "./App";
+import Counter from "./components/Counter/Counter";
+
+ReactDOM.render(
+  // <App />,
+  <Counter />,
+
+  document.getElementById("root")
+);
